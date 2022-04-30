804bf283ee109975af7932e2646c13586eca4413
youngmin <ymkwon3@gnu.ac.kr>
2022-04-30 17:23:55 +0900

    set permit

--- src/shared/App.js
+++ src/shared/App.js
@@ -2,15 +2,18 @@ import React from "react";
 import "./App.css";
 import { Flex } from "../elements";
 import { Route } from "react-router";
-import Main from '../pages/Main';
-import Login from '../pages/Login';
-import MyPage from '../pages/MyPage';
+import Main from "../pages/Main";
+import Login from "../pages/Login";
+import MyPage from "../pages/MyPage";
+import Permit from "./Permit";
 
 const App = () => {
   return (
-    <Flex styles={{ height: "100vh"}}>
-      <Route path="/" exact component={Main}></Route>
-      <Route path="/mypage" exact component={MyPage}></Route>
+    <Flex styles={{ height: "100vh" }}>
+      <Permit>
+        <Route path="/" exact component={Main}></Route>
+        <Route path="/mypage" exact component={MyPage}></Route>
+      </Permit>
       <Route path="/login" exact component={Login}></Route>
     </Flex>
   );
