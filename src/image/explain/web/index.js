16bfaa4c3ddda768ec70de48a22d41ff03a72442
youngmin <ymkwon3@gnu.ac.kr>
2022-05-24 23:36:06 +0900

    web explain image

---
+++ src/image/explain/web/index.js
@@ -0,0 +1,12 @@
+import start from "./start.png";
+import host1 from "./host1.png";
+import host2 from "./host2.png";
+import host3 from "./host3.png";
+import host4 from "./host4.png";
+import parti1 from "./parti1.png";
+import parti2 from "./parti2.png";
+import parti3 from "./parti3.png";
+import parti4 from "./parti4.png";
+import end from "./end.png";
+
+export {start, host1, host2, host3, host4, parti1, parti2, parti3, parti4, end}
