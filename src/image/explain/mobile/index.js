974e22012df19142128cbedba4e6b809cfc32bd9
suchan <scdreamricher@gmail.com>
2022-05-25 11:56:20 +0900

    Create mobile modal page of explanation for how to use our product.

---
+++ src/image/explain/mobile/index.js
@@ -0,0 +1,23 @@
+import mstart from "./mstart.png";
+import mhost1 from "./mhost1.png";
+import mhost2 from "./mhost2.png";
+import mhost3 from "./mhost3.png";
+import mhost4 from "./mhost4.png";
+import mparti1 from "./mparti1.png";
+import mparti2 from "./mparti2.png";
+import mparti3 from "./mparti3.png";
+import mparti4 from "./mparti4.png";
+import mend from "./mend.png";
+
+export {
+  mstart,
+  mhost1,
+  mhost2,
+  mhost3,
+  mhost4,
+  mparti1,
+  mparti2,
+  mparti3,
+  mparti4,
+  mend,
+};
